4d279030298426facce889f0c35d12c8bd976bd6
Mvke-IT <michael.duarte09@gmail.com>
2024-01-16 13:30:01 -0400

    took out the last for loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,31 @@
 #include "sort.h"
+
 /**
- * bubble_sort - Sorts an array of integers in ascending orderusing the Bubble sort algorithm
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
+ * bubble_sort - sort an array in ascending order
+ * @array: pointer to the array to sort
+ * @size: size of the array
+*/
+
 void bubble_sort(int *array, size_t size)
 {
-    int swapped, temp;
-    unsigned int index, yndex;
+	int swapped, temp;
+	unsigned int index, yndex;
 
-    for (index = 0; index < (size - 1); index++)
-    {
-        swapped = 0;
-        for (yndex = 0; yndex < (size - index - 1); yndex++)
-        {
-            if (array[yndex] > array[yndex + 1])
-            {
-                temp = array[yndex];
-                array[yndex] = array[yndex + 1];
-                array[yndex + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-        if (swapped == 0)
-            break;
-    }
+	for (index = 0; index < (size - 1); index++)
+	{
+		swapped = 0;
+		for (yndex = 0; yndex < (size - index - 1); yndex++)
+		{
+			if (array[yndex] > array[yndex + 1])
+			{
+				temp = array[yndex];
+				array[yndex] = array[yndex + 1];
+				array[yndex + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
