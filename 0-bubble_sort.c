818d9c7234e9d41a54b703a6e60a451ef8690658
Mvke-IT <michael.duarte09@gmail.com>
2024-01-17 17:22:18 -0400

    changed variables and second if statement

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,39 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort an array in ascending order
- * @array: pointer to the array to sort
- * @size: size of the array
-*/
-
+ * bubble_sort - Sorts array of integers in ascending order using Bubble Sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	int swapped, temp;
-	unsigned int index, yndex;
+	size_t i, j;
+	int temp;
+	int swapped;
 
-	for (index = 0; index < (size - 1); index++)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-		for (yndex = 0; yndex < (size - index - 1); yndex++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[yndex] > array[yndex + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[yndex];
-				array[yndex] = array[yndex + 1];
-				array[yndex + 1] = temp;
-				print_array(array, size);
+				/* Swap the two elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				swapped = 1;
+
+				/* Print the array after each swap */
+				print_array(array, size);
 			}
 		}
+
+		/* If no two elements were swapped in inner loop, the array is sorted */
 		if (swapped == 0)
 			break;
 	}
