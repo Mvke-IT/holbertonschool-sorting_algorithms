631c18315a1577939da5cf515557cc3b789d7e72
Mvke-IT <michael.duarte09@gmail.com>
2024-01-18 10:05:35 -0400

    added two prototypes

--- sort.h
+++ sort.h
@@ -38,13 +38,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-<<<<<<< HEAD
 void quick_recursion(int *array, int low, int high, size_t size);
 int division(int *array, int low, int high, size_t size);
-=======
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void swap_ints(int *a, int *b);
 void print_list(const listint_t *list);
->>>>>>> ee1ba620012a068b7b79f04163c81c37849a38cd
 
 #endif /* SORT_H */
