cc02469e62fa05768b7c0da794403ae9e4041623
Mvke-IT <michael.duarte09@gmail.com>
2024-01-18 14:24:14 -0400

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,7 +27,6 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swapped = 1;
-
 				/* Print the array after each swap */
 				print_array(array, size);
 			}
