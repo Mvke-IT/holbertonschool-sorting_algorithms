cfce164b98b28d118c31784f16b1b36827072bc0
Mvke-IT <michael.duarte09@gmail.com>
2024-01-18 10:09:30 -0400

    added two prototypes

--- sort.h
+++ sort.h
@@ -1,19 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
-
-/**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -24,24 +13,20 @@ typedef enum bool
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
-/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+
 void quick_recursion(int *array, int low, int high, size_t size);
 int division(int *array, int low, int high, size_t size);
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
-void swap_ints(int *a, int *b);
-void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif
